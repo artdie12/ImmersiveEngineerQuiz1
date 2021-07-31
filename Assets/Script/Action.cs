68bdff745231fb522c66bb5bf16e00d6897d9bd8
Radian <54185603+artdie12@users.noreply.github.com>
2021-07-31 09:34:43 +0700

    first
    init

---
+++ Assets/Script/Action.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+
+    public GameObject objAction;
+    Vector3 scaleStart;
+    public float minScale;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleStart = objAction.transform.localScale;
+    }
+
+    Vector2 t1start, t0start;
+    Vector2 rotationStart;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+
+
+
+            if (Input.touchCount == 1)
+            {
+                Touch t = Input.GetTouch(0);
+
+                if (t.phase == TouchPhase.Moved)
+                {
+                    objAction.transform.Rotate(new Vector3( t.deltaPosition.y, t.deltaPosition.x,0),90,Space.World);
+
+
+                }
+
+            }
+            else if (Input.touchCount == 2)
+            {
+
+                Touch t0 = Input.GetTouch(0);
+                Touch t1 = Input.GetTouch(1);
+
+                if (t0.phase == TouchPhase.Began)
+                {
+                    t0start = t0.position;
+                }
+                if (t1.phase == TouchPhase.Began)
+                {
+                    t1start = t1.position;
+                }
+
+
+                if (t0.phase == TouchPhase.Moved || t1.phase == TouchPhase.Moved)
+                {
+                    float selisih= t1.position.x + t1.position.y - (t0.position.x - t0.position.y);
+                    float selisihStart = t1start.x - t0start.x + t1start.y - t0start.y;
+                    float deltaselisih = (t0.deltaPosition.x + t0.deltaPosition.y) - (t1.deltaPosition.x + t1.deltaPosition.y);
+                    if (selisih > 0)
+                    {  
+                        objAction.transform.localScale += new Vector3 (deltaselisih,deltaselisih,deltaselisih );
+                    }
+                    else if (selisih<0)
+                    {
+                        objAction.transform.localScale -= new Vector3(deltaselisih, deltaselisih, deltaselisih);
+                    }
+                    
+                    Vector2 distanceTouchStart = new Vector2(Mathf.Abs(t1start.x - t0start.x), Mathf.Abs(t1start.y - t0start.y));
+                    Vector2 distanceTouch = new Vector2(Mathf.Abs(t1.position.x - t0.position.x), Mathf.Abs(t1.position.y - t0.position.y));
+                    float totalDistance = distanceTouch.x + distanceTouch.y - (distanceTouchStart.x + distanceTouchStart.y);
+
+                    if (objAction.transform.localScale.x > scaleStart.x * minScale)
+                    {
+                       objAction.transform.localScale = scaleStart +  (scaleStart * totalDistance*0.1f);
+                    }
+
+                }
+
+            }
+
+        }
+
+        
+
+    }
+
+
+
+    //blinking 
+
+    float blinktime;
+    public float blinkDuration,blinkdistence;
+    bool isBlinking = false;
+    public void doBlink()
+    {
+
+        if (isBlinking)
+        {
+            if (blinktime < blinkDuration)
+            {
+                blinktime += Time.deltaTime;
+            }
+            else
+            {
+                objAction.SetActive(true);
+                blinktime = 0;
+                isBlinking = false;
+            }
+        }
+        else
+        {
+            if (blinktime < blinkdistence)
+            {
+                blinktime += Time.deltaTime;
+            }
+            else
+            {
+                objAction.SetActive(false);
+                blinktime = 0;
+                isBlinking = true;
+            }
+        }
+
+
+    
+    }
+
+
+   
+}
+
---
+++ Assets/Script/SSandShare.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SSandShare : MonoBehaviour
+{
+
+    public Button shareButton;
+
+    private bool isFocus = false;
+
+    private string shareSubject, shareMessage;
+    private bool isProcessing = false;
+    private string screenshotName;
+
+    void Start()
+    {
+        shareButton.onClick.AddListener(OnShareButtonClick);
+    }
+
+
+    void OnApplicationFocus(bool focus)
+    {
+        isFocus = focus;
+    }
+
+    public void OnShareButtonClick()
+    {
+
+        screenshotName = "ArutalaSs.png";
+        shareSubject = "Arutala Logo";
+        shareMessage = "it is Logo";
+
+        ShareScreenshot();
+    }
+
+
+    private void ShareScreenshot()
+    {
+
+
+        if (!isProcessing)
+        {
+            StartCoroutine(ShareScreenshotInAnroid());
+        }
+    }
+
+    public IEnumerator ShareScreenshotInAnroid()
+    {
+
+        isProcessing = true;
+        // wait for graphics to render
+        yield return new WaitForEndOfFrame();
+
+        string screenShotPath = Application.persistentDataPath + "/" + screenshotName;
+        ScreenCapture.CaptureScreenshot(screenshotName, 1);
+        yield return new WaitForSeconds(0.5f);
+
+        if (!Application.isEditor)
+        {
+            //Create intent for action send
+            AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+            AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+            intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+
+            //create image URI to add it to the intent
+            AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+            AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + screenShotPath);
+
+            //put image and string extra
+            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+            intentObject.Call<AndroidJavaObject>("setType", "image/png");
+            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), shareSubject);
+            intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareMessage);
+
+            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share yours");
+            currentActivity.Call("startActivity", chooser);
+        }
+
+        yield return new WaitUntil(() => isFocus);
+        isProcessing = false;
+    }
+
+}
